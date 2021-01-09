b76a3b5a4cae2a120799ce12792fd1558f2eefee
Default-loves <2111605130@mail2.gdut.edu.cn>
2021-01-09 18:12:59 +0800

    update

--- Algorithm/src/com/junyi/LeetCodeSolution.java
+++ Algorithm/src/com/junyi/LeetCodeSolution.java
@@ -15,69 +15,57 @@ import static java.lang.Math.min;
 
 public class LeetCodeSolution {
 
-    public List<String> fullJustify(String[] words, int maxWidth) {
-        ArrayList<String> res = new ArrayList<>();
-        int i = 0;
-        int n = words.length;
-        while (i < n) {
-            // j指向下一个单词，cnt累计字符数量，每行至少能够放置一个单词
-            int j = i+1;
-            int cnt = words[i].length();
-            while (j < n && cnt + words[j].length() + 1 <= maxWidth) {
-                cnt += words[j++].length() + 1;
-            }
-            String tmp = resolveLine(words, i, j-1, cnt, maxWidth);
-            res.add(tmp);
-            i = j;
-        }
+    public List<Integer> splitIntoFibonacci(String S) {
+        ArrayList<Integer> res = new ArrayList<>();
+        doSplit(res, S, 0, S.length(), 0, 0);
         return res;
     }
 
-    private String resolveLine(String[] words, int l, int r, int length, int maxWidth) {
-        // 单词个数
-        int countWord = r - l + 1;
-        // 空格数量
-        int countSpace = maxWidth - length + countWord - 1;
-        // 平均每个单词之间空格的数量
-        int avgSpace = countWord == 1 ? 0 : countSpace/(countWord - 1);
-        // 空格数量不均匀，前面的 extraSpace 个空格位置需要多放置一个空格
-        int extraSpace = countWord == 1 ? 0 : countSpace % (countWord-1);
-        StringBuilder sb = new StringBuilder();
-        // 处理最后一行或者单行一个单词
-        if (r == words.length-1 || l == r) {
-            while (l <= r) {
-                sb.append(words[l++]).append(" ");
-            }
-            // 由于上面每个单词后面都跟了一个空字符，如果行末是非空字符，即单词刚刚好，那么我们就多添加了一个空字符，需要删除
-            if (sb.length() > maxWidth) {
-                sb.deleteCharAt(sb.length()-1);
-            }
-            sb.append(generateEmpty(maxWidth-sb.length()));
-            return sb.toString();
+    /**
+     *
+     * @param res：最后的结果
+     * @param source：源字符串, 不会改变
+     * @param index：当前的字符串索引
+     * @param length：源字符串的长度
+     * @param sum：切分结果最后两个元素的和
+     * @param prev：切分结果最后一个元素的值
+     */
+    private boolean doSplit(ArrayList<Integer> res, String source, int index, int length, int sum, int prev) {
+        if (index == length) {
+            return res.size() >= 3;
         }
-        int start = l;
-        while (l < r) {
-            int k = (l-start) < extraSpace ? 1 : 0;
-            sb.append(words[l++]);
-            sb.append(generateEmpty(avgSpace + k));
+        long cnt = 0;
+        for (int i = index; i < length; i++) {
+             if (index < i && source.charAt(index) == '0') {   // 对于开头是“0”的子串，不再处理
+                 break;
+             }
+             cnt = cnt * 10 + (source.charAt(i) - '0');
+             if (cnt > Integer.MAX_VALUE) {     // 每个子串的大小不大于2^31 - 1
+                 break;
+             }
+             int cur = (int) cnt;
+             if (res.size() >= 2) {
+                 if (cur < sum) {
+                     continue;
+                 } else if (cur > sum) {    // 累加的值已经大于切分结果最后两个元素的和，后续不再处理
+                     break;
+                 }
+             }
+             res.add(cur);      // 添加进 res 的数符合 cur == sum
+             if (doSplit(res, source, i+1, length, prev + cur, cur)) {
+                 return true;
+             } else {
+                 res.remove(res.size()-1);
+             }
         }
-        sb.append(words[l]);
-        return sb.toString();
+        return false;
     }
-
-    /** 生成k个空字符(' ')的字符串 */
-    private String generateEmpty(Integer k) {
-        char[] chars = new char[k];
-        Arrays.fill(chars, ' ');
-        return String.valueOf(chars);
-    }
-
 
 
     public static void main(String[] argv) {
         LeetCodeSolution lcs = new LeetCodeSolution();
         String[] arr = new String[]{"What","must","be","acknowledgment","shall","be"};
-        List<String> res = lcs.fullJustify(arr, 16);
+        List<Integer> res = lcs.splitIntoFibonacci("123456579");
         System.out.println(res);
     }
 }
