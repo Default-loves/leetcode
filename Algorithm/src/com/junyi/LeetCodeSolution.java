be522d4a71699a0aa829145e86e0ed75cb6a3d50
Default-loves <2111605130@mail2.gdut.edu.cn>
2021-01-29 18:16:27 +0800

    update

--- Algorithm/src/com/junyi/LeetCodeSolution.java
+++ Algorithm/src/com/junyi/LeetCodeSolution.java
@@ -4,6 +4,7 @@ import javafx.util.Pair;
 import org.junit.platform.commons.util.StringUtils;
 
 import java.util.*;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -15,29 +16,29 @@ import static java.lang.Math.min;
 
 public class LeetCodeSolution {
 
-    public int findTargetSumWays(int[] nums, int S) {
-        int sum = Arrays.stream(nums).sum();
-        if (Math.abs(S) > Math.abs(sum)) {
-            return 0;
+    public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
+        int N = group.length;
+        int THRESHOLD = 1_000_000_007;
+        int[][] dp = new int[n+1][minProfit+1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
         }
-        int t = sum*2+1;    // dp的列数，包括了负数、0、正数
-        int[][] dp = new int[nums.length+1][t];
-        dp[0][sum] = 1;     // 初始化
-        for (int i = 1; i <= nums.length; i++) {
-            for (int j = 0; j < t; j++) {
-                int part1 = j - sum - nums[i-1] >= 0? dp[i-1][j-sum-nums[i-1]]: 0;
-                int part2 = j - sum + nums[i-1] >= 0? dp[i-1][j-sum+nums[i-1]]: 0;
-                dp[i][j] = part1 + part2;
+        for (int i = 0; i < N; i++) {
+            for (int j = n; j >= group[i]; j--) {
+                for (int k = minProfit; k >= 0; k--) {
+                    int t = Math.max(k - profit[i], 0);
+                    dp[j][k] += dp[j-group[i]][t];
+                    dp[j][k] %= THRESHOLD;
+                }
             }
         }
-        return dp[nums.length][S];
+        return dp[n][minProfit];
     }
-
 
     public static void main(String[] argv) {
         LeetCodeSolution lcs = new LeetCodeSolution();
-        String[] arr = new String[]{"What","must","be","acknowledgment","shall","be"};
-        List<Integer> res = lcs.splitIntoFibonacci("123456579");
+        int[] array = new int[]{1, 1, 1, 1, 1};
+        Integer res = lcs.findTargetSumWays(array, 3);
         System.out.println(res);
     }
 }
