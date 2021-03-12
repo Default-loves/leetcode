e6807edd14c98d8f159a296b652e0e927240dab3
Default-loves <2111605130@mail2.gdut.edu.cn>
2021-03-12 18:07:24 +0800

    update

--- Algorithm/src/com/junyi/LeetCodeSolution.java
+++ Algorithm/src/com/junyi/LeetCodeSolution.java
@@ -18,26 +18,49 @@ import static java.lang.Math.*;
 public class LeetCodeSolution {
 
 
-    public int minSetSize(int[] arr) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i : arr) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
-        }
-        ArrayList<Integer> list = new ArrayList<>(map.values());
-        Collections.sort(list, (a, b) ->  (b - a));     // 降序排序
-
-        int result = 0;
-        int cnt = 0;
-        int halfLen = arr.length >> 1;
-        for (int i = 0; i < list.size(); i++) {
-            cnt += list.get(i);
-            result++;
-            if (cnt > halfLen) {
-                return result;
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        for (int num : nums) {
+            int length = result.size();
+            for (int i = 0; i < length; i++) {
+                ArrayList<Integer> item = new ArrayList<>(result.get(i));
+                item.add(num);
+                if (!result.contains(item)) {
+                    result.add(item);
+                }
             }
         }
-        return halfLen;
+        return result;
     }
+
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<>());// 初始化空数组
+        Arrays.sort(nums);
+        int start = 1; //保存新解的开始位置
+        for (int i = 0; i < nums.length; i++) {
+            List<List<Integer>> ans_tmp = new ArrayList<>();
+            // 遍历之前的所有结果
+            for (int j = 0; j < ans.size(); j++) {
+                List<Integer> list = ans.get(j);
+                //如果出现重复数字，就跳过所有旧解
+                if (i > 0 && nums[i] == nums[i - 1] && j < start) {
+                    continue;
+                }
+                List<Integer> tmp = new ArrayList<>(list);
+                tmp.add(nums[i]); // 加入新增数字
+                ans_tmp.add(tmp);
+            }
+
+            start = ans.size(); //更新新解的开始位置
+            ans.addAll(ans_tmp);
+        }
+        return ans;
+    }
+
+
 
     public static void main(String[] argv) {
         LeetCodeSolution lcs = new LeetCodeSolution();
